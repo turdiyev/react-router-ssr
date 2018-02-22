327af6463385c7c2e8d83219981f8f3fbab6ab8f
Sherali <sherali.uz@gmail.com>
2018-02-22 14:25:28 +0500

    feat(App.js): Routers

--- App.js
+++ App.js
@@ -1,4 +1,59 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+class Menu extends Component {
+	render() {
+		return (
+			<div>
+				<ul>
+					<li>
+						<Link to={'/'}>Homepage</Link>
+					</li>
+					<li>
+						<Link to={'/about'}>About</Link>
+					</li>
+					<li>
+						<Link to={'/contact'}>Contact</Link>
+					</li>
+				</ul>
+			</div>
+		);
+	}
+}
+class Homepage extends Component {
+	render() {
+		return (
+			<div>
+                <Menu></Menu>
+				<h1>Homepage</h1>
+			</div>
+		);
+	}
+}
+
+class About extends Component {
+	render() {
+		return (
+			<div>
+                 <Menu></Menu>
+				<h1>About</h1>
+			</div>
+		);
+	}
+}
+
+class Contact extends Component {
+	render() {
+		return (
+			<div>
+                 <Menu></Menu>
+				<h1>Contact</h1>
+			</div>
+		);
+	}
+}
+
 
 export default class App extends Component {
 	constructor(props) {
@@ -8,7 +63,11 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<h1>Hello World!</h1>
+				<Switch>
+					<Route exact path='/' component={ Homepage } />
+					<Route path="/about" component={ About } />
+					<Route path="/contact" component={ Contact } />
+				</Switch>
 			</div>
 		);
 	}
